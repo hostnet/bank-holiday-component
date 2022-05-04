a833ab92d93085da7a122a09a09dc51e40f18ffe
Jan Lam <jlam@hostnet.nl>
2022-05-04 16:14:46 +0200

    Update phpcs-tool to 9.1.0 with Hostnet-Level-1

--- src/BankHoliday.php
+++ src/BankHoliday.php
@@ -15,58 +15,58 @@ final class BankHoliday
     /**
      * The first day of a year.
      */
-    const NEW_YEARS_DAY = 'new_years_day';
+    public const NEW_YEARS_DAY = 'new_years_day';
 
     /**
      * The sunday of easter.
      */
-    const EASTER_SUNDAY = 'easter_sunday';
+    public const EASTER_SUNDAY = 'easter_sunday';
 
     /**
      * The monday of easter.
      */
-    const EASTER_MONDAY = 'easter_monday';
+    public const EASTER_MONDAY = 'easter_monday';
 
     /**
      * The Dutch bank holiday Kings day.
      */
-    const DUTCH_KINGS_DAY = 'dutch_kings_day';
+    public const DUTCH_KINGS_DAY = 'dutch_kings_day';
 
     /**
      * Bank holiday on May 5th, denoting the end of WW2 in The Netherlands.
      * This is, for the majority of the Dutch population, a bank holiday once every five years.
      */
-    const DUTCH_LIBERATION_DAY = 'dutch_liberation_day';
+    public const DUTCH_LIBERATION_DAY = 'dutch_liberation_day';
 
     /**
      * Ascension day (Christian holiday).
      */
-    const ASCENSION_DAY = 'ascension_day';
+    public const ASCENSION_DAY = 'ascension_day';
 
     /**
      * Whit sunday (Christian holiday).
      */
-    const WHIT_SUNDAY = 'whit_sunday';
+    public const WHIT_SUNDAY = 'whit_sunday';
 
     /**
      * Whit monday is a bank holiday that follows the Christian holiday whit sunday.
      */
-    const WHIT_MONDAY = 'whit_monday';
+    public const WHIT_MONDAY = 'whit_monday';
 
     /**
      * Christmas day, always on December 25th.
      */
-    const CHRISTMAS_DAY = 'christmas_day';
+    public const CHRISTMAS_DAY = 'christmas_day';
 
     /**
      * The day after christmas day.
      */
-    const BOXING_DAY = 'boxing_day';
+    public const BOXING_DAY = 'boxing_day';
 
     /**
      * All bank holidays that are defined in this class.
      */
-    const ALL = [
+    public const ALL = [
         self::DUTCH_LIBERATION_DAY,
         self::ASCENSION_DAY,
         self::BOXING_DAY,
@@ -82,7 +82,7 @@ final class BankHoliday
     /**
      * Set of Dutch bank holidays.
      */
-    const DUTCH_BANK_HOLIDAY_SET = [
+    public const DUTCH_BANK_HOLIDAY_SET = [
         self::NEW_YEARS_DAY,
         self::EASTER_MONDAY,
         self::DUTCH_KINGS_DAY,
